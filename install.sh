af3a995a795c1e9984d208429f6bb4b63449ff85
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2021-09-21 09:18:57 +0900

    fix: install script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 cd ~/catkin_ws/src
 git clone https://github.com/YDLIDAR/ydlidar_ros.git
 git clone https://github.com/aamirhatim/twist_filter.git
+git clone https://github.com/KMiyawaki/oit_roboclaw_driver.git
 cd ~/catkin_ws/src/ydlidar_ros
 git checkout refs/tags/1.4.1
 sudo apt-get install -y ros-melodic-teleop-twist-keyboard
@@ -9,3 +10,6 @@ sudo apt-get install -y ros-melodic-key-teleop
 sudo apt-get install -y xterm
 cd ~/catkin_ws
 catkin_make
+cd ~/catkin_ws/src/ydlidar_ros/startup
+sudo ./initenv.sh
+sudo reboot
