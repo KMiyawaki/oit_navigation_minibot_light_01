eb3c5f9223903661bbe2527c5a1cda2f7e1fdb31
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2022-12-11 12:29:48 +0900

    fix: YDlidar repo is deleted

--- install.sh
+++ install.sh
@@ -6,19 +6,21 @@ function main(){
     local -r WS_SETUP="~/catkin_ws/devel/setup.bash"
     source ${ROS_SETUP}
     source ${WS_SETUP}
+    cd ~/catkin_ws
+    catkin_make
     cd ~/catkin_ws/src
-    git clone https://github.com/YDLIDAR/ydlidar_ros.git
     git clone https://github.com/aamirhatim/twist_filter.git
     git clone https://github.com/KMiyawaki/oit_roboclaw_driver.git
-    cd ~/catkin_ws/src/ydlidar_ros
-    git checkout refs/tags/1.4.1
+    # git clone https://github.com/YDLIDAR/ydlidar_ros.git
+    # cd ~/catkin_ws/src/ydlidar_ros
+    # git checkout refs/tags/1.4.1
     sudo apt-get install -y ros-melodic-teleop-twist-keyboard
     sudo apt-get install -y ros-melodic-key-teleop
     sudo apt-get install -y xterm
     cd ~/catkin_ws
     catkin_make
-    cd ~/catkin_ws/src/ydlidar_ros/startup
-    sudo ./initenv.sh
+    # cd ~/catkin_ws/src/ydlidar_ros/startup
+    # sudo ./initenv.sh
 }
 
 main "$@"
