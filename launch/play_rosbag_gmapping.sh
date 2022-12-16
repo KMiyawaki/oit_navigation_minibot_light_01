a5301bde1efb9cb35d3a4b5b83166b34ae0e1a11
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2022-12-16 12:33:42 +0900

    add: rosbag tools

---
+++ launch/play_rosbag_gmapping.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function main() {
+    local -r PKG="oit_navigation_minibot_light_01"
+    local -r LAUNCH="play_rosbag_gmapping.launch"
+    local -r PARAM_NAME="bag"
+    local -r MIN_ARG=1
+    if [ $# -lt "${MIN_ARG}" ]; then
+        echo "${FUNCNAME[0]} 引数は ${MIN_ARG} 個以上必要です。" 1>&2
+        echo "usage: ${FUNCNAME[0]} [bag filename]" 1>&2
+        return 1
+    fi
+    
+    local -r BAG_FILE=`basename ${1}`
+    local -r ADDITIONAL="${@:2:($#-1)}"
+    local -r CMD="roslaunch ${PKG} ${LAUNCH} ${PARAM_NAME}:=${BAG_FILE} ${ADDITIONAL}"
+    # echo ${CMD}
+    eval ${CMD}
+}
+
+main "$@"
